81502e6d9d3d1df06b0581c565ca913e56216bba
Tyler M. <J.t.moore4700@gmail.com>
2024-12-01 21:44:00 +0000

    work on setting up basic webpage

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use salvo::prelude::*;
 
-#[handler]
-async fn hello()-> &'static str {
-    "Hello, Sera"
-}
-
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new().get(hello);
-    let acceptor = Tcplistener::new("127.0.0.1:5000").bind().await;
+    let router = Router::with_path("/workspaces/MarketTrack/pkg").get(
+        StaticDir::new([
+            "/workspaces/MarketTrack/pkg/index.html"
+        ])
+        .defaults("index.html")
+        .auto_list(true),
+    );
+    let acceptor = TcpListener::new("127.0.0.1:5000").bind().await;
     Server::new(acceptor).serve(router).await;
 }
