5b2704b857d2ede91a9dddb82cf29577b173cc5d
Tyler M. <J.t.moore4700@gmail.com>
2024-12-01 20:54:55 +0000

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("");
+use salvo::prelude::*;
+
+#[handler]
+async fn hello()-> &'static str {
+    "Hello, Sera"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let router = Router::new().get(hello);
+    let acceptor = Tcplistener::new("127.0.0.1:5000").bind().await;
+    Server::new(acceptor).serve(router).await;
 }
